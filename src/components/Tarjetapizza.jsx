523b3d7c145574d68b63d62e6128d1d39ce2f624
Alexanderalavi <alexanderalaviperez@gmail.com>
2025-05-31 15:09:13 -0400

    listo hito 1

--- src/components/Tarjetapizza.jsx
+++ src/components/Tarjetapizza.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 const Tarjetapizza = (props) => {
   return (
-    <div class="card" style={{ width: '28rem' }}>
+    <div class="card" style={{ width: '32rem' }}>
       <img src={props.imagen} class="card-img-top" alt={props.alternativo} />
       <div class="card-body">
         <h5 class="card-title">{props.nombre}</h5>
         <hr class="linea" />
-        <section>
+        <section class="ingredientes">
         <p>Ingredientes:</p>
         <p>🍕{props.ingredientes.join(" , ")}</p>
         </section>
          <hr class="linea" /> 
-         <h5>Precio: ${props.precio}</h5>
+         <h5 class="precio">Precio: ${props.precio}</h5>
         <div class="botoneria">
-            <button type="button" class="btn btn-light">Ver Más 👀</button>
+            <button type="button" class="btn btn-outline-dark">Ver Más 👀</button>
             <button type="button" class="btn btn-dark">añadir🛒</button>
 
         </div>

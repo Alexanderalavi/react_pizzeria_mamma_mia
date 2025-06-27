260cb6cdcac3f9db47c8612e557eaf5d296cdcdb
Alexanderalavi <alexanderalaviperez@gmail.com>
2025-06-26 23:27:42 -0400

    hito 5 listo

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <div>
-      NotFound
-    </div>
+ <section className="d-flex flex-column justify-content-center align-items-center text-center vh-100">
+  <h2 className="mb-4">Link equivocado! Volver al menu principal:</h2>
+  <Link to='/'><img className='tower' src="src/assets/img/tower.webp" alt="Hora Clinic" /></Link>
+ </section>
   )
 }
 
--- src/views/ProfilePage.jsx
+++ src/views/ProfilePage.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 const ProfilePage = () => {
+
+  const userEmail = 'PEPPINO@PIZZATOWER.COM'
+
+
   return (
-    <div>
-      Perfil y correo wewewewe
-    </div>
-  )
-}
+    <section className="container my-5 text-center">
+      <h2 className='fw-bolder text-uppercase'>Tu Perfil</h2>
+      <p>Email: <span className='fw-bolder'>{userEmail}</span></p>
+      <button className="btn btn-primary mt-3">
+        Cerrar Sesión
+      </button>
+    </section>
+  );
+};
 
 export default ProfilePage

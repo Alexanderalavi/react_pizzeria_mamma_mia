3efdb2e072068a661abec05a3cb3d9fda97c1091
Alexanderalavi <alexanderalaviperez@gmail.com>
2025-07-03 20:44:33 -0400

    carro modficado

--- src/views/CartPizza.jsx
+++ src/views/CartPizza.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react';
-import { pizzaCart } from '../data/pizzas';
+import { useCart } from '../context/CartContext';
+
 
 const CartPizza = () => {
-  const [cart, setCart] = useState(pizzaCart)
-  const cantidadPizza = (id, variacion) => {setCart(prev => prev.map(pizza =>pizza.id === id ? { ...pizza, count: pizza.count + variacion } : pizza).filter(pizza => pizza.count > 0))}
-  const total = cart.reduce((guardado, pizza) => guardado + pizza.price * pizza.count, 0)
+ const { carro, agregarCarro, removerCarro, suma } = useCart()
 
   return (
     <div className="container mt-4">
       <h2 className="mb-4">Detalles del pedido:</h2>
 
-      {cart.length ? (<section>{cart.map(({ id, img, name, price, count }) => (
+      {carro.length ? (<section>{carro.map(({ id, img, name, price, count }) => (
             <div key={id} className="d-flex align-items-center justify-content-between mb-3 border-bottom pb-2">
               <div className="d-flex align-items-center gap-3">
                 <img
@@ -28,20 +26,20 @@ const CartPizza = () => {
 
               <div className="d-flex align-items-center gap-2">
 
-                <button className="btn btn-outline-danger btn-sm" onClick={() => cantidadPizza(id, -1)}>
+                <button className="btn btn-outline-danger btn-sm"  onClick={() => removerCarro(id)}>
                   -
                 </button>
 
                 <span className="fw-bold">{count}</span>
 
-                <button className="btn btn-outline-primary btn-sm" onClick={() => cantidadPizza(id, 1)}>
+                <button className="btn btn-outline-primary btn-sm"  onClick={() => agregarCarro({ id, img, name, price })}>
                   +
                 </button>
               </div>
             </div> ))}
 
           <div className="text-start mt-4 mb-4">
-            <h4>Total: ${total}</h4>
+            <h4>Total: ${suma}</h4>
             <button className="btn btn-dark mt-2">Pagar</button>
           </div>
         </section>
